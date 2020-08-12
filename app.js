717a6cf81c5d487e376efa24293c9b7ea430291c
Arpan Sharma <35891294+arpan-sharma26@users.noreply.github.com>
2020-08-13 00:33:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const canva = document.getElementById('one');
-const canva1 = document.getElementById('two');
-const canva2 = document.getElementById('three');
-
-$('.carousel').carousel({
-    interval: 2000
-  })
-  
-  let arp = player.getPlayerState();
-  let res = parseInt(arp);
-  alert(res);
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
