bc5a4729b35a660db638f236c61c89cd81d1d0f0
Arpan Sharma <35891294+arpan-sharma26@users.noreply.github.com>
2020-08-12 17:47:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const canva = document.getElementById('one');
+const canva1 = document.getElementById('two');
+const canva2 = document.getElementById('three');
+
+$('.carousel').carousel({
+    interval: 2000
+  })
+  
+  let arp = player.getPlayerState();
+  let res = parseInt(arp);
+  alert(res);
